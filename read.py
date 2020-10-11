fc5f4bdeccebbea3cbf23a48f408c8ee264b26e5
Anna <anna.ovsi@mail.ru>
2020-10-11 19:52:14 +0300

    in developing..

--- read.py
+++ read.py
@@ -22,18 +22,18 @@ def search_number(video, name="test"):
         #fps = int(cap.get(cv2.CAP_PROP_FPS))
         #out = cv2.VideoWriter(PATH + name + "_detect.mp4", cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (w, h))
         ret = True
+    x = []
+    y = []
     while ret:
         ret, frame = cap.read()
-        x = []
-        y = []
         cadr = 0
         if ret:
+            cadr += 1
             state, cords = load_model.detect_number(frame)
             if state:
+                logging.debug('Нашли номер на %d кадре' %cadr)
                 for c in cords:
-                    cadr += 1
-                    logging.info(c)
-                    logging.info(c[0])
+                    logging.info('Координаты' + str(c[0]))
                     x1 = c[0][0]
                     x2 = c[1][0]
                     x3 = c[2][0]
@@ -44,16 +44,15 @@ def search_number(video, name="test"):
                     y4 = c[3][1]
                     x_mean = (x1+x2+x3+x4)/4
                     y_mean = (y1+y2+y3+y4)/4
+                    logging.info('Координаты середины номера %f %f' % (x_mean, y_mean))
                     x.append(x_mean)
                     y.append(y_mean)
-                    plt.hist2d(x, y, bins=10)
-
-
+                    plt.hist2d(x, y, bins=32)
                     pts = np.array(c, np.int32)
                     pts = pts.reshape((-1, 1, 2))
                     cv2.polylines(frame, [pts], True, (255, 0, 0), 2)
                     frame = cv2.circle(frame, (int(x_mean), int(y_mean)), 3, (0,0,255), thickness=1)
-            plt.savefig('video/test' + str(cadr))
+            plt.savefig('video/test_' + str(cadr))
             #out.write(frame)
     cap.release()
     cv2.destroyAllWindows()
