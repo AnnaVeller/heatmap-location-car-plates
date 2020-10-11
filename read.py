b5b78552c68b50e4268a903178035915f3b86cfd
Anna <anna.ovsi@mail.ru>
2020-10-11 18:54:25 +0300

    in developing..

--- read.py
+++ read.py
@@ -29,9 +29,20 @@ def search_number(video, name="test"):
                 for c in cords:
                     logging.info(c)
                     logging.info(c[0])
-                    logging.info(c[1])
+                    x1 = c[0][0]
+                    x2 = c[1][0]
+                    x3 = c[2][0]
+                    x4 = c[3][0]
+                    y1 = c[0][1]
+                    y2 = c[1][1]
+                    y3 = c[2][1]
+                    y4 = c[3][1]
+                    x_mean = (x1+x2+x3+x4)/4
+                    y_mean = (y1+y2+y3+y4)/4
                     pts = np.array(c, np.int32)
                     pts = pts.reshape((-1, 1, 2))
                     cv2.polylines(frame, [pts], True, (255, 0, 0), 2)
+                    cv2.circle(frame, (x_mean, y_mean), 3, (0,0,255), thickness=1, lineType=8, shift=0)
+            out.write(frame)
     cap.release()
     cv2.destroyAllWindows()
