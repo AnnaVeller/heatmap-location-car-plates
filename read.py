cf787d85266a7c46e14aca18e51ddacf7c7cf2b4
Anna <anna.ovsi@mail.ru>
2020-10-11 20:16:19 +0300

    in developing..

--- read.py
+++ read.py
@@ -51,7 +51,7 @@ def search_number(video, name="test"):
                     log.info('Координаты середины номера %f %f' % (x_mean, y_mean))
                     x.append(x_mean)
                     y.append(h-y_mean)
-                    plt.hist2d(x, y, bins=(w,h), range=range)
+                    plt.hist2d(x, y, bins=100, range=range)
                     pts = np.array(c, np.int32)
                     pts = pts.reshape((-1, 1, 2))
                     cv2.polylines(frame, [pts], True, (255, 0, 0), 2)
