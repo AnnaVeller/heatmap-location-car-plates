f628f23be07b323989039bf7252bc487b5a2b06b
Anna <anna.ovsi@mail.ru>
2020-10-11 19:58:56 +0300

    in developing..

--- read.py
+++ read.py
@@ -19,8 +19,8 @@ def search_number(video, name="test"):
         log.debug("Unable to read video")
         ret = False
     else:
-        #h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        #w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         #fps = int(cap.get(cv2.CAP_PROP_FPS))
         #out = cv2.VideoWriter(PATH + name + "_detect.mp4", cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), fps, (w, h))
         ret = True
@@ -49,7 +49,7 @@ def search_number(video, name="test"):
                     log.info('Координаты середины номера %f %f' % (x_mean, y_mean))
                     x.append(x_mean)
                     y.append(y_mean)
-                    plt.hist2d(x, y, bins=32)
+                    plt.hist2d(x, y, bins=32, xedges=w, yedges=h)
                     pts = np.array(c, np.int32)
                     pts = pts.reshape((-1, 1, 2))
                     cv2.polylines(frame, [pts], True, (255, 0, 0), 2)
