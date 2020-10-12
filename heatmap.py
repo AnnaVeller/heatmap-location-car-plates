d4d1425d921c05b1728a6e72b82ad140a54eaa5b
Anna <anna.ovsi@mail.ru>
2020-10-12 10:17:37 +0300

    in developing..

--- heatmap.py
+++ heatmap.py
@@ -10,14 +10,9 @@ logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s')
 log = logging.getLogger('Heatmap')
 log.setLevel(logging.DEBUG)
 
-PATH_VIDEO = "video1.mp4"
+PATH_VIDEO = "test_mini4.mp4"
 NAME = os.path.splitext(PATH_VIDEO)[0]
 log.info(" Запустили видео %s" % PATH_VIDEO)
 read.search_number(PATH_VIDEO, NAME)
 log.info(" Закрыли видео %s/n/n" % PATH_VIDEO)
 
-PATH_VIDEO = "video2.mp4"
-NAME = os.path.splitext(PATH_VIDEO)[0]
-log.info(" Запустили видео %s" % PATH_VIDEO)
-read.search_number(PATH_VIDEO, NAME)
-log.info(" Закрыли видео %s/n/n" % PATH_VIDEO)
