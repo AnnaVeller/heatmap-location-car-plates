87cb07f438da8cad219aad836ef520b1cc8010d4
Anna <anna.ovsi@mail.ru>
2020-10-12 11:32:34 +0300

    in developing..

--- draw.py
+++ draw.py
@@ -6,9 +6,8 @@ def draw_hist(x,y, range, bins=(11,11), name='test'):
     #plt.savefig('video/' + name, bbox_inches='tight')
 
     H, xedges, yedges, img = plt.hist2d(x, y)
-    extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    im = ax.imshow(H, cmap=plt.cm.jet, extent=extent)
+    im = ax.imshow(H, cmap=plt.cm.jet, extent=range)
     fig.colorbar(im, ax=ax)
     plt.savefig('video/' + name)
--- heatmap.py
+++ heatmap.py
@@ -18,13 +18,13 @@ x,y, range = read.search_number(PATH_VIDEO, NAME)
 log.info(" Закрыли видео %s \n\n" % PATH_VIDEO)
 draw.draw_hist(x,y, range)
 
-PATH_VIDEO = "test_mini2.mp4"
-bins = (9, 9)
-NAME = os.path.splitext(PATH_VIDEO)[0]
-log.info(" Запустили видео %s" % PATH_VIDEO)
-x,y, range = read.search_number(PATH_VIDEO, NAME)
-log.info(" Закрыли видео %s \n\n" % PATH_VIDEO)
-draw.draw_hist(x,y, range)
+#PATH_VIDEO = "test_mini2.mp4"
+#bins = (9, 9)
+#NAME = os.path.splitext(PATH_VIDEO)[0]
+#log.info(" Запустили видео %s" % PATH_VIDEO)
+#x,y, range = read.search_number(PATH_VIDEO, NAME)
+#log.info(" Закрыли видео %s \n\n" % PATH_VIDEO)
+#draw.draw_hist(x,y, range)
 
 
 #PATH_VIDEO = "test_mini4.mp4"
