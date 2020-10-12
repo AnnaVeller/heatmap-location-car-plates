092157eb3e41236562e96b7b1101d22b683e7410
Anna <anna.ovsi@mail.ru>
2020-10-12 11:55:45 +0300

    in developing..

--- draw.py
+++ draw.py
@@ -4,3 +4,5 @@ def draw_hist(x,y, range, bins=(11,11), name='test'):
     hist = plt.hist2d(x, y, bins=bins, range=range)
     plt.colorbar(hist[3])
     plt.savefig('video/' + name, bbox_inches='tight')
+    plt.clf()
+    plt.cla()
