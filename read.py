e8339b67e11b8297e7eec99c9cae7c2ddecd381c
Anna <anna.ovsi@mail.ru>
2020-10-12 14:47:17 +0300

    in developing..

--- read.py
+++ read.py
@@ -7,7 +7,7 @@ log = logging.getLogger('Heatmap')
 log.setLevel(logging.DEBUG)
 
 PATH = "/home/user/repos/heatmap-location-car-plates/video/"
-SEC_TO_WRITE = 0.9
+SEC_TO_WRITE = 0.5
 
 
 def search_number(video, name="test"):
