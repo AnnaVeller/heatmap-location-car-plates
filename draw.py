3b132cc527bb74a9b54d8ee5d2373801db05c3fa
Anna <anna.ovsi@mail.ru>
2020-10-12 13:47:46 +0300

    in developing..

--- draw.py
+++ draw.py
@@ -1,19 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def draw_hist(x,y, range, bins=(11,11), name='test'):
-    range = ((0, w), (0, h))
-    hist = plt.hist2d(x, y, bins=bins, range=range)
-    plt.colorbar(hist[3])
-    plt.savefig('video/' + name, bbox_inches='tight')
-    plt.clf()
-    plt.cla()
-
-
 PATH = "/home/user/repos/heatmap-location-car-plates/video/"
 file = open(PATH+'test_multy.txt')
 w, h, name, fps = file[0].split()
 x = []
 y = []
 for line in file[1:]:
-    x.append()
+    x1,x2,x3,x4,y1,y2,y3,y4 = line.split()
+    x.extend([x1,x2,x3,x4])
+    y.extend([y1,y2,y3,y4])
+
+bins=(11, 11)
+hist = plt.hist2d(x, y, bins=bins, range=range)
+plt.colorbar(hist[3])
+plt.savefig('video/' + name, bbox_inches='tight')
+plt.clf()
+plt.cla()
