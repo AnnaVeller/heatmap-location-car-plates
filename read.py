d70ccd790a727253a28722d534e416dc2a46fd09
Anna <anna.ovsi@mail.ru>
2020-10-12 13:13:29 +0300

    in developing..

--- read.py
+++ read.py
@@ -7,10 +7,10 @@ log = logging.getLogger('Heatmap')
 log.setLevel(logging.DEBUG)
 
 PATH = "/home/user/repos/heatmap-location-car-plates/video/"
-
+SEC_TO_WRITE = 0.5
 
 def search_number(video, name="test"):
-    file = open(name + '.txt', 'a')
+    file = open(PATH + name + '.txt', 'a')
     cap = cv2.VideoCapture(video)
     if not cap.isOpened():
         log.debug("Unable to read video")
@@ -18,29 +18,26 @@ def search_number(video, name="test"):
     else:
         h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        file.write('%d %d %s' % (w,h,name))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+        file.write('%d %d %s %d \n' % (w, h, name, fps))
         log.debug('Видео [%dx%d]' % (w, h))
         ret = True
     cadr = 0
-    delta_time = 0
-    time_between = 0
+    cadr_to_write = SEC_TO_WRITE*fps
     while ret:
         ret, frame = cap.read()
         cadr += 1
         if ret:
             length = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000
-            delta_time = length - delta_time
-            if time_between >= 0.5:
+            log.debug(" Прошло: %f sec" % length)
+            if cadr % cadr_to_write == 0:
+                log.debug(" Ищем на: %f sec" % length)
                 state, cords = load_model.detect_number(frame)
                 if state:
                     log.debug('Нашли номер на %d кадре' % cadr)
                     for c in cords:
                         log.info('Координаты' + str(c[0]))
-                        file.write(str(c))
-                time_between = 0
-            else:
-                time_between = time_between + length - delta_time
-            log.debug(" Прошло: %f sec, между обработанными кадрами %f sec" % (length, time_between))
+                        file.write(str(c)+'\n')
     file.close()
     cap.release()
     cv2.destroyAllWindows()
