05ba0d0a28cc674bbd2dd60d2a0f177df7da6506
Anna <anna.ovsi@mail.ru>
2020-10-12 13:54:58 +0300

    in developing..

--- draw.py
+++ draw.py
@@ -3,15 +3,17 @@ import matplotlib.pyplot as plt
 
 PATH = "/home/user/repos/heatmap-location-car-plates/video/"
 file = open(PATH+'test_multy.txt')
-#print(file[0])
-#[w, h, name, fps] = file[0].split()
+line = file.readline()
+w, h, name, fps = line.split()
 x = []
 y = []
-for line in file:
+while line:
     print(line)
     x1,x2,x3,x4,y1,y2,y3,y4 = line.split()
     x.extend([x1,x2,x3,x4])
     y.extend([y1,y2,y3,y4])
+    line = file.readline()
+file.close()
 
 bins=(11, 11)
 hist = plt.hist2d(x, y, bins=bins, range=range)
