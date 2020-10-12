d25ed0927394a2eaefb677b4ea02288e4bac0044
Anna <anna.ovsi@mail.ru>
2020-10-12 14:04:28 +0300

    in developing..

--- draw.py
+++ draw.py
@@ -9,6 +9,7 @@ x = []
 y = []
 while line:
     line = file.readline()
+    print("output", line)
     x1,x2,x3,x4,y1,y2,y3,y4 = line.split()
     x.extend([x1,x2,x3,x4])
     y.extend([y1,y2,y3,y4])
